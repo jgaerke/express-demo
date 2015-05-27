2dae6a694604ca911e6af581520a0562ce7fe5c2
unknown <kon5575@OF060PK0V5XP7.kroger.com>
2015-05-27 14:29:59 -0400

    expose 'profile' resource

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 //require dependencies
 var express = require('express'),
-    fs = require('fs');
+    fs = require('fs'),
+    _ = require('lodash');
 
 //instantiate an app instance
 var app = express();
@@ -17,6 +18,24 @@ app.get('/', function (req, res) {
 app.get('/api/profiles', function (req, res) {
   fs.readFile('Data.json', 'utf8', function (err, data) {
     return res.json(JSON.parse(data));
+  });
+});
+
+app.get('/api/profiles/:id', function (req, res) {
+  fs.readFile('Data.json', 'utf8', function (err, data) {
+
+    data = JSON.parse(data)
+    data = _.findWhere(data, {id: req.params.id});
+
+    if (!data) {
+      return res.status(422).json({
+        code: 422,
+        message: 'Unprocessable Entity',
+        reason: 'The profile you requested does not exist.'
+      });
+    }
+
+    return res.json(data);
   });
 });
 
