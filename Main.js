cb6bd80e111fafa039095e079931cd0490ee9567
unknown <kon5575@OF060PK0V5XP7.kroger.com>
2015-05-27 14:27:51 -0400

    expose 'profile' resource collection.

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 //require dependencies
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 //instantiate an app instance
 var app = express();
@@ -10,6 +11,13 @@ app.set('json spaces', 2);
 //expose hello world
 app.get('/', function (req, res) {
   res.send('hello world');
+});
+
+//expose 'profiles' collection
+app.get('/api/profiles', function (req, res) {
+  fs.readFile('Data.json', 'utf8', function (err, data) {
+    return res.json(JSON.parse(data));
+  });
 });
 
 //start application by listening on port 3000
